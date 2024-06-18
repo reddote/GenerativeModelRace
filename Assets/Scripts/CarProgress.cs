913c8de6b9a770ca7b1ab4a8e0ff8ec6578abb13
MUHSIN\muhsin <muhsinalperasar@kist.re.kr>
2024-06-18 16:30:48 +0900

    added position tracker

--- Assets/Scripts/CarProgress.cs
+++ Assets/Scripts/CarProgress.cs
@@ -2,41 +2,41 @@ using UnityEngine;
 
 public class CarProgress : MonoBehaviour
 {
-    public int currentCheckpointIndex = 0;
-    public Transform nextCheckpoint;
-    private RaceManager raceManager;
+	public int currentCheckpointIndex = 0;
+	public Transform nextCheckpoint;
+	private RaceManager raceManager;
 
-    void Start()
-    {
-        raceManager = FindObjectOfType<RaceManager>();
-        UpdateNextCheckpoint();
-    }
+	void Start()
+	{
+		raceManager = FindObjectOfType<RaceManager>();
+		UpdateNextCheckpoint();
+	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Checkpoint") && other.transform == nextCheckpoint)
-        {
-            currentCheckpointIndex++;
-            UpdateNextCheckpoint();
-            raceManager.UpdateCarProgress(this);
-        }
-    }
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Checkpoint") && other.transform == nextCheckpoint)
+		{
+			currentCheckpointIndex++;
+			UpdateNextCheckpoint();
+			// Notify race manager (not necessary if updating in Update in RaceManager)
+			// raceManager.UpdateCarProgress(this);
+		}
+	}
 
-    void UpdateNextCheckpoint()
-    {
-        // Assuming checkpoints are stored in an array in the race manager
-        if (currentCheckpointIndex < raceManager.checkpoints.Length)
-        {
-            nextCheckpoint = raceManager.checkpoints[currentCheckpointIndex];
-        }
-    }
+	void UpdateNextCheckpoint()
+	{
+		if (currentCheckpointIndex < raceManager.checkpoints.Length)
+		{
+			nextCheckpoint = raceManager.checkpoints[currentCheckpointIndex];
+		}
+	}
 
-    public float GetDistanceToNextCheckpoint()
-    {
-        if (nextCheckpoint != null)
-        {
-            return Vector3.Distance(transform.position, nextCheckpoint.position);
-        }
-        return float.MaxValue;
-    }
+	public float GetDistanceToNextCheckpoint()
+	{
+		if (nextCheckpoint != null)
+		{
+			return Vector3.Distance(transform.position, nextCheckpoint.position);
+		}
+		return float.MaxValue;
+	}
 }
--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 
 public class RaceManager : MonoBehaviour
 {
-    public Transform[] checkpoints;
-    private List<CarProgress> cars = new List<CarProgress>();
+	public Transform[] checkpoints;
+	public CarProgress playerCar;
+	private List<CarProgress> cars;
+	[SerializeField] private TextMeshProUGUI trackPosition;
+	[SerializeField] private TextMeshProUGUI allCarCount;
 
-    void Start()
-    {
-        // Initialize the list of cars
-        cars = FindObjectsOfType<CarProgress>().ToList();
-    }
+	void Start()
+	{
+		cars = FindObjectsOfType<CarProgress>().ToList();
+		allCarCount.text = "2";
+	}
 
-    public void UpdateCarProgress(CarProgress car)
-    {
-        // Sort cars by checkpoint index and then by distance to next checkpoint
-        cars = cars.OrderByDescending(c => c.currentCheckpointIndex)
-            .ThenBy(c => c.GetDistanceToNextCheckpoint())
-            .ToList();
+	void LateUpdate()
+	{
+		UpdateCarPositions();
+	}
 
-        // Display the current order
-        for (int i = 0; i < cars.Count; i++)
-        {
-            Debug.Log($"Car {cars[i].name} is in position {i + 1}");
-        }
-    }
+	void UpdateCarPositions()
+	{
+		// Sort cars by checkpoint index and then by distance to next checkpoint
+		cars = cars.OrderByDescending(c => c.currentCheckpointIndex)
+			.ThenBy(c => c.GetDistanceToNextCheckpoint())
+			.ToList();
+
+		// Display the current order
+		for (int i = 0; i < cars.Count; i++)
+		{
+			if (cars[i] == playerCar){
+				trackPosition.text = (i + 1) + "";
+			}
+			Debug.Log($"Car {cars[i].name} is in position {i + 1}");
+		}
+	}
 }
